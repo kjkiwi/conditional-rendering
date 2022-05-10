b953b6df72226fcbbd299d19652b6f46d478f226
Minty <82933018+kjkiwi@users.noreply.github.com>
2022-05-11 11:05:46 +1200

    Create conditional function Create function to render different pages depending on login status. Attempt to feed into App() doesn't work because the function is a statement (if-else) and needs to be an expression (reduce to a value)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import Login from "./Login";
+//to change screen shown depending on Login status
+var isLoggedIn = false;
 
-function App() {
+//using if-else 
+function renderConditionally() {
+if (isLoggedIn) {
+  return <h1>Hello</h1>
+} else {
   return <Login />;
+}
+}
+
+//doesn't work to sub function into App because it is not an expression, it is a statement. Need to use ternary operator to format it as an expression
+function App() {
+  return {renderConditionally()}
 }
 
 export default App;
