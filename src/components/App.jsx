b7685433b1b1ce774dc53cec59ad7b81759cd21f
Minty <82933018+kjkiwi@users.noreply.github.com>
2022-05-11 11:42:36 +1200

    Ternary operator with action only if condition is truthy Check if time is before 12 noon and if it is, show <h1>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,16 @@ import Login from "./Login";
 //to change screen shown depending on Login status
 var isLoggedIn = false;
 
-//function to get current time
+//function to get current hour as a number
 const currentTime = new Date().getHours();
 console.log(currentTime);
 
-//renderConditionally() converted to expression so it can be passed inline to App()
+//ternary operator when you only want an action if condition is truthy, and no action if condition is falsy
 function App() {
   return (
     <div className="container">
-      {isLoggedIn ? <h1>Hello</h1> : <Login />}{" "}
-      {/* this is the renderConditionally() function */}
+      {currentTime < 12 ? <h1> Time for lunch </h1> : null}
+      {/* {isLoggedIn ? <h1>Hello</h1> : <Login />}{" "} */}
     </div>
   );
 }
