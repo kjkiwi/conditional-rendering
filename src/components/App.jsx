429d820f20feab1c579540a08f83ae5213c57a4e
Minty <82933018+kjkiwi@users.noreply.github.com>
2022-05-11 11:17:33 +1200

    Conditional function passed inline to App() Need to reformat it using ternary operator CONDITION ? EXPRESSION IF TRUTHY : EXPRESSION IF FALSY

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,14 @@ import Login from "./Login";
 //to change screen shown depending on Login status
 var isLoggedIn = false;
 
-//using if-else
-function renderConditionally() {
-  if (isLoggedIn) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Login />;
-  }
-}
-
-//works to sub in renderConditionally() function, just not in-line, unless it is in expression format
+//renderConditionally() converted to expression so it can be passed inline to App()
 function App() {
-  return <div className="container">{renderConditionally()}</div>;
+  return (
+    <div className="container">
+      {isLoggedIn ? <h1>Hello</h1> : <Login />}{" "}
+      {/* this is the renderConditionally() function */}
+    </div>
+  );
 }
 
 export default App;
