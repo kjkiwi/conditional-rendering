c2228bd7cb8a2c27bd689cf6ee7625805aaa9e9f
Minty <82933018+kjkiwi@users.noreply.github.com>
2022-05-11 11:10:28 +1200

    Actually works to sub in function to App() Can sub in a function to another function, just cant do it inline in JSX unless it is in the format of an expression (i.e. reduces to a value)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,21 @@
 import React from "react";
+import { render } from "react-dom";
 import Login from "./Login";
 //to change screen shown depending on Login status
 var isLoggedIn = false;
 
-//using if-else 
+//using if-else
 function renderConditionally() {
-if (isLoggedIn) {
-  return <h1>Hello</h1>
-} else {
-  return <Login />;
-}
+  if (isLoggedIn) {
+    return <h1>Hello</h1>;
+  } else {
+    return <Login />;
+  }
 }
 
-//doesn't work to sub function into App because it is not an expression, it is a statement. Need to use ternary operator to format it as an expression
+//works to sub in renderConditionally() function, just not in-line, unless it is in expression format
 function App() {
-  return {renderConditionally()}
+  return <div className="container">{renderConditionally()}</div>;
 }
 
 export default App;
